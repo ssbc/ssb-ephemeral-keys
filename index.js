796637f8e587ddb283280e5b97aff242e4d7c9dc
ameba23 <ameba23@systemli.org>
2019-03-28 20:38:56 +0100

    move crypto stuff into seperate file

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 const mkdirp = require('mkdirp')
 const { join } = require('path')
 const level = require('level')
-const sodium = require('sodium-native')
 const { assert, isString, isObject, isFunction } = require('./util')
 
-const secretBox = sodium.crypto_secretbox_easy
-const secretBoxOpen = sodium.crypto_secretbox_open_easy
-const NONCEBYTES = sodium.crypto_secretbox_NONCEBYTES
-const KEYBYTES = sodium.crypto_secretbox_KEYBYTES
-const zero = sodium.sodium_memzero
-const concat = Buffer.concat
 const curve = 'curve25519'
 const defaultContextMessage = 'SSB Ephemeral key'
 const cipherTextSuffix = '.box'
+
+const { keyPair, decryptMessage, encryptMessage } = require('./crypto')
 
 module.exports = {
   name: 'ephemeral',
@@ -57,7 +52,7 @@ module.exports = {
       assert(isString(contextMessageString), 'Context message must be a string')
       const contextMessage = Buffer.from(contextMessageString, 'utf-8')
 
-      callback(null, encryptMessage(pubKey, messageBuffer, contextMessage))
+      callback(null, encryptMessage(pubKey, messageBuffer, contextMessage) + cipherTextSuffix)
     }
 
     function unBoxMessage (dbKey, cipherTextBase64, contextMessageString, callback) {
@@ -125,59 +120,6 @@ module.exports = {
       deleteKeyPair
     }
   }
-}
-
-function randomBytes (n) {
-  var b = Buffer.alloc(n)
-  sodium.randombytes_buf(b)
-  return b
-}
-
-function genericHash (msg, key) {
-  var hash = sodium.sodium_malloc(sodium.crypto_generichash_BYTES_MAX)
-  sodium.crypto_generichash(hash, msg, key)
-  return hash
-}
-
-function scalarMult (sk, pk) {
-  var result = sodium.sodium_malloc(sodium.crypto_scalarmult_BYTES)
-  sodium.crypto_scalarmult(result, sk, pk)
-  return result
-}
-
-function keyPair () {
-  var ephKeypair = {}
-  ephKeypair.publicKey = sodium.sodium_malloc(KEYBYTES)
-  ephKeypair.secretKey = sodium.sodium_malloc(KEYBYTES)
-  sodium.crypto_box_keypair(ephKeypair.publicKey, ephKeypair.secretKey)
-  return ephKeypair
-}
-
-function encryptMessage (pubKey, messageBuffer, contextMessage) {
-  var boxed = Buffer.alloc(messageBuffer.length + sodium.crypto_secretbox_MACBYTES)
-  const ephKeypair = keyPair()
-  const nonce = randomBytes(NONCEBYTES)
-  var sharedSecret = genericHash(
-    concat([ ephKeypair.publicKey, pubKey, contextMessage ]),
-    genericHash(scalarMult(ephKeypair.secretKey, pubKey)))
-
-  secretBox(boxed, messageBuffer, nonce, sharedSecret)
-
-  zero(sharedSecret)
-  zero(ephKeypair.secretKey)
-  return concat([nonce, ephKeypair.publicKey, boxed]).toString('base64') + cipherTextSuffix
-}
-
-function decryptMessage (ephKeypair, pubKey, box, unboxed, contextMessage, nonce) {
-  var sharedSecret = genericHash(
-    concat([ pubKey, ephKeypair.publicKey, contextMessage ]),
-    genericHash(scalarMult(ephKeypair.secretKey, pubKey)))
-
-  const success = secretBoxOpen(unboxed, box, nonce, sharedSecret)
-  zero(sharedSecret)
-  zero(ephKeypair.secretKey)
-  zero(ephKeypair.publicKey)
-  return success ? unboxed.toString() : false
 }
 
 const packKey = k => k.toString('base64') + '.' + curve
